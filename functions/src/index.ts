5ac48036dc54fc03ce2fc00c9ddb3b64b21ee52f
Angelo <Angelo.van.osch@hotmail.com>
2023-11-17 21:16:55 +0100

    fixed part of the cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
 
-exports.updateEmail = functions.https.onCall(async (data, context) => {
-  functions.logger.log('test');
-});
+exports.updateEmail = functions
+  .region('europe-west1')
+  .https.onRequest(async (data, context) => {
+    functions.logger.log('test');
+  });
